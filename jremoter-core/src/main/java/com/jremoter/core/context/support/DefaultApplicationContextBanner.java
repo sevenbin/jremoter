2470217bdc4b87262ef81695a538fb74b28a2c4b
tb_bin@163.com <tb_bin@163.com>
2018-02-01 16:40:13 +0800

    Bean功能完善

--- jremoter-core/src/main/java/com/jremoter/core/context/support/DefaultApplicationContextBanner.java
+++ jremoter-core/src/main/java/com/jremoter/core/context/support/DefaultApplicationContextBanner.java
@@ -11,15 +11,15 @@ public class DefaultApplicationContextBanner implements ApplicationContextBanner
 	@Override
 	public void write(PrintStream printStream){
 		printStream.println("----------------------------------------------------------------------");
-		printStream.println("                 _ _____                      _            ");
-		printStream.println("                | |  __ \\                    | |           ");
-		printStream.println("                | | |__) |___ _ __ ___   ___ | |_ ___ _ __ ");
-		printStream.println("            _   | |  _  // _ \\ '_ ` _ \\ / _ \\| __/ _ \\ '__|");
-		printStream.println("           | |__| | | \\ \\  __/ | | | | | (_) | ||  __/ |   ");
-		printStream.println("            \\____/|_|  \\_\\___|_| |_| |_|\\___/ \\__\\___|_|   ");
-		printStream.println("                                                            ");
+		printStream.println("                 _ _____                      _                       ");
+		printStream.println("                | |  __ \\                    | |                      ");
+		printStream.println("                | | |__) |___ _ __ ___   ___ | |_ ___ _ __            ");
+		printStream.println("            _   | |  _  // _ \\ '_ ` _ \\ / _ \\| __/ _ \\ '__|           ");
+		printStream.println("           | |__| | | \\ \\  __/ | | | | | (_) | ||  __/ |              ");
+		printStream.println("            \\____/|_|  \\_\\___|_| |_| |_|\\___/ \\__\\___|_|              ");
+		printStream.println("                                                                      ");
 		printStream.println("----------------------------------------------------------------------");
-		printStream.println("                            KoKo  v0.0.5                    ");
+		printStream.println("                            KoKo  v0.0.5                              ");
 		printStream.println("----------------------------------------------------------------------");
 	}
 	
